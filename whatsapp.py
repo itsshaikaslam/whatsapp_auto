0d0a8ca6f95e47d165342e9c730f2289a9c02e50
Aslam Shaik <31707911+itsshaikaslam@users.noreply.github.com>
2020-06-06 19:51:21 +0530

    Second commit message

--- whatsapp.py
+++ whatsapp.py
@@ -1,7 +1,7 @@
 #print("Hello ASma AShaaz")
 
 from twilio.rest import Client 
- 
+##Second commit message
 account_sid = 'AC96d7c9577e19f6a0eb5550b426bad749' 
 auth_token = '0bb9c3657273efa1ee8aed167121ffcd' 
 client = Client(account_sid, auth_token) 
